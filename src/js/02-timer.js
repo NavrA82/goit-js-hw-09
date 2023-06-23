39a90c6461138ac66637f7328289b4b02fb142a4
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-24 00:03:47 +0300

    center-menu

---
+++ src/js/02-timer.js
@@ -0,0 +1,6 @@
+const ref = {
+  bodyView: document.querySelector('body'),
+};
+
+ref.bodyView.style.cssText = `background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;
+flex-direction: column;`;
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,9 @@
+const ref = {
+  bodyView: document.querySelector('body'),
+};
+ref.bodyView.style.cssText = `background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;
+flex-direction: column;`;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
