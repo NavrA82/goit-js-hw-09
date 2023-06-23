08de77b61bbfd3ff6dc70efbc393e07e8e1c8c8c
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-23 23:51:36 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+const ref = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+  bodyView: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+ref.buttonStart.style.cssText = `background-color: #4d5ae5; font-size: large; color: #44cc1a; padding: 20px 40px; border: 2px solid #44cc1a; border-radius: 8px; cursor: pointer;`;
+ref.buttonStop.style.cssText = `background-color: #4d5ae5; font-size: large; color: #f6c218; padding: 20px 40px; border: 2px solid #f6c218;  border-radius: 8px; cursor: pointer;`;
+
+ref.bodyView.style.cssText = `background-color: ${getRandomHexColor()}; display: flex; gap: 20px; align-items: center;
+flex-direction: column;`;
+
+ref.buttonStart.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    ref.bodyView.style.cssText = `background-color: ${getRandomHexColor()}; display: flex; gap: 20px; align-items: center;
+flex-direction: column;`;
+  }, 1000);
+
+  ref.buttonStart.disabled = true;
+  ref.buttonStop.disabled = false;
+
+  ref.buttonStart.style.cssText = `background-color: rgba(239, 239, 239, 0.3); font-size: large; color: rgba(16, 16, 16, 0.3), rgba(255, 255, 255, 0.3); padding: 20px 40px; border: 2px solid rgba(118, 118, 118, 0.3), rgba(195, 195, 195, 0.3); border-radius: 8px; `;
+  ref.buttonStop.style.cssText = `background-color: #4d5ae5; font-size: large; color: #f6c218; padding: 20px 40px; border: 2px solid #f6c218; border-radius: 8px; cursor: pointer;`;
+});
+
+ref.buttonStop.addEventListener('click', () => {
+  clearInterval(intervalId);
+
+  ref.buttonStart.disabled = false;
+  ref.buttonStop.disabled = true;
+
+  ref.buttonStart.style.cssText = `background-color: #4d5ae5; font-size: large; color: #44cc1a; padding: 20px 40px; border: 2px solid #44cc1a; border-radius: 8px; cursor: pointer;`;
+  ref.buttonStop.style.cssText = `background-color: rgba(239, 239, 239, 0.3); font-size: large; color:  rgba(16, 16, 16, 0.3), rgba(255, 255, 255, 0.3); padding: 20px 40px; border: 2px solid rgba(118, 118, 118, 0.3), rgba(195, 195, 195, 0.3);  border-radius: 8px;`;
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
