49ff7a559fdbedb2c0d8898075a07cd4ea14f2b7
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-24 14:09:34 +0000

    Deploying to gh-pages from @ NavrA82/goit-js-hw-09@2d0cf64a9cbca576e81e0a9918a34f93aa7ea517 🚀

--- 02-timer.33a9e934.js
+++
@@ -1,2 +0,0 @@
-({bodyView:document.querySelector("body")}).bodyView.style.cssText="background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;\nflex-direction: column;";
-//# sourceMappingURL=02-timer.33a9e934.js.map
--- 02-timer.fca9d07b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body").style.cssText="background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;\nflex-direction: column;";
-//# sourceMappingURL=02-timer.fca9d07b.js.map
