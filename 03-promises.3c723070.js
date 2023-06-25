788547b729d88e70ce9b035230373c246d21f0a5
NavrA82 <124286244+NavrA82@users.noreply.github.com>
2023-06-25 18:17:12 +0000

    Deploying to gh-pages from @ NavrA82/goit-js-hw-09@21f2a98d3ef7bd949271cc4748752becca156a29 🚀

--- 03-promises.3c723070.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("body").style.cssText="background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;\nflex-direction: column;";
-//# sourceMappingURL=03-promises.3c723070.js.map
--- 03-promises.9bf2e582.js
+++
@@ -1,2 +0,0 @@
-({bodyView:document.querySelector("body")}).bodyView.style.cssText="background-color: #a9a8a8; display: flex; gap: 20px; align-items: center;\nflex-direction: column;";
-//# sourceMappingURL=03-promises.9bf2e582.js.map
